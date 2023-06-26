83ddc90b90f456830049a2adeee69d199b87f73f
blebihan2023 <blebihan2023@12SE22-3TJRZS2.ad.campus-eni.fr>
2023-06-26 10:18:33 +0200

    Initialisation du projet

---
+++ ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/ProjetEncheresGroupe1Application.java
@@ -0,0 +1,13 @@
+package fr.eni.groupe1.encheres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEncheresGroupe1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetEncheresGroupe1Application.class, args);
+	}
+
+}
