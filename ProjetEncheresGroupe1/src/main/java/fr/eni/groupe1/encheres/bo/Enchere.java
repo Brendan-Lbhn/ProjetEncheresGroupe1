d79ebc9eb3e10bf1ee934825c2eba64e4f9224c4
tgarciapardo2023 <tgarciapardo2023@12SE22-CKLRZS2.ad.campus-eni.fr>
2023-06-26 14:40:52 +0200

    bo enchere modifié

--- ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/bo/Enchere.java
+++ ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/bo/Enchere.java
@@ -1,6 +1,7 @@
 package fr.eni.groupe1.encheres.bo;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Enchere {
 
@@ -26,10 +27,28 @@ public class Enchere {
 	public Integer getMontant_enchere() {
 		return montant_enchere;
 	}
-
+	
 	public void setMontant_enchere(Integer montant_enchere) {
 		this.montant_enchere = montant_enchere;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dateEnchere, montant_enchere);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Enchere other = (Enchere) obj;
+		return Objects.equals(dateEnchere, other.dateEnchere) && Objects.equals(montant_enchere, other.montant_enchere);
+	}
+
 
 	@Override
 	public String toString() {
