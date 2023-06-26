39fa129f2c4fd67469915c24c3ad25f94c8e5f93
blebihan2023 <blebihan2023@12SE22-3TJRZS2.ad.campus-eni.fr>
2023-06-26 14:28:45 +0200

    BO Utilisateur ok

--- ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/bo/Utilisateur.java
+++ ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/bo/Utilisateur.java
@@ -1,5 +1,154 @@
 package fr.eni.groupe1.encheres.bo;
 
+import java.util.Objects;
+
 public class Utilisateur {
+	
+	private Integer noUtilisateur; 
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone; 
+	private String rue;
+	private Integer codePostal;
+	private String ville;
+	private String motDePasse;
+	private Integer credit;
+	private String administrateur;
+	
+	public Utilisateur() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getNoUtilisateur() {
+		return noUtilisateur;
+	}
+
+	public void setNoUtilisateur(Integer noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public Integer getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(Integer codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+	public Integer getCredit() {
+		return credit;
+	}
+
+	public void setCredit(Integer credit) {
+		this.credit = credit;
+	}
+
+	public String getAdministrateur() {
+		return administrateur;
+	}
+
+	public void setAdministrateur(String administrateur) {
+		this.administrateur = administrateur;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(administrateur, codePostal, credit, email, motDePasse, noUtilisateur, nom, prenom, pseudo,
+				rue, telephone, ville);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Utilisateur other = (Utilisateur) obj;
+		return Objects.equals(administrateur, other.administrateur) && Objects.equals(codePostal, other.codePostal)
+				&& Objects.equals(credit, other.credit) && Objects.equals(email, other.email)
+				&& Objects.equals(motDePasse, other.motDePasse) && Objects.equals(noUtilisateur, other.noUtilisateur)
+				&& Objects.equals(nom, other.nom) && Objects.equals(prenom, other.prenom)
+				&& Objects.equals(pseudo, other.pseudo) && Objects.equals(rue, other.rue)
+				&& Objects.equals(telephone, other.telephone) && Objects.equals(ville, other.ville);
+	}
+
+	@Override
+	public String toString() {
+		return "Utilisateur [noUtilisateur=" + noUtilisateur + ", pseudo=" + pseudo + ", nom=" + nom + ", prenom="
+				+ prenom + ", email=" + email + ", telephone=" + telephone + ", rue=" + rue + ", codePostal="
+				+ codePostal + ", ville=" + ville + ", motDePasse=" + motDePasse + ", credit=" + credit
+				+ ", administrateur=" + administrateur + "]";
+	}
+
+	
+	
 
 }
