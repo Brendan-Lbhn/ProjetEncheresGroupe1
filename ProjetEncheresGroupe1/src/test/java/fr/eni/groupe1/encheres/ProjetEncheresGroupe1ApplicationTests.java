6384c10e0daf0d9fa3b4f977a4002a55c313470a
blebihan2023 <blebihan2023@12SE22-3TJRZS2.ad.campus-eni.fr>
2023-06-26 11:11:14 +0200

    Test ajout page Html

---
+++ ProjetEncheresGroupe1/src/test/java/fr/eni/groupe1/encheres/ProjetEncheresGroupe1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.groupe1.encheres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEncheresGroupe1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
