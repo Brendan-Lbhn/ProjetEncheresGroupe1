29ff3eed159306517e6884a4c02a606173e8c870
tgarciapardo2023 <tgarciapardo2023@12SE22-CKLRZS2.ad.campus-eni.fr>
2023-06-27 11:27:30 +0200

    controller+service+dal utilisateur

---
+++ ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/bll/UtilisateurServiceImpl.java
@@ -0,0 +1,16 @@
+package fr.eni.groupe1.encheres.bll;
+
+import org.springframework.stereotype.Service;
+
+import fr.eni.groupe1.encheres.bo.Utilisateur;
+
+@Service
+public class UtilisateurServiceImpl implements UtilisateurService {
+
+	@Override
+	public void enregistrer(Utilisateur utilisateur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/dal/UtilisateurDAO.java
@@ -0,0 +1,7 @@
+package fr.eni.groupe1.encheres.dal;
+
+import fr.eni.groupe1.encheres.bo.Utilisateur;
+
+public interface UtilisateurDAO {
+	void save(Utilisateur utilisateur);
+}
