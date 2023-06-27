e0fe10da1d51b6ba205b3831fe3ac1af0cb0f9e6
tgarciapardo2023 <tgarciapardo2023@12SE22-CKLRZS2.ad.campus-eni.fr>
2023-06-27 14:04:32 +0200

    Modification dal+controller+bll Utilisateur

--- ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/bll/UtilisateurServiceImpl.java
+++ ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/bll/UtilisateurServiceImpl.java
@@ -3,14 +3,19 @@ package fr.eni.groupe1.encheres.bll;
 import org.springframework.stereotype.Service;
 
 import fr.eni.groupe1.encheres.bo.Utilisateur;
+import fr.eni.groupe1.encheres.dal.UtilisateurDAO;
 
 @Service
 public class UtilisateurServiceImpl implements UtilisateurService {
+	private UtilisateurDAO utilisateurDAO;
+	
+	public UtilisateurServiceImpl(UtilisateurDAO utilisateurDAO) {
+		this.utilisateurDAO=utilisateurDAO;
+	}
 
 	@Override
 	public void enregistrer(Utilisateur utilisateur) {
-		// TODO Auto-generated method stub
-		
+		utilisateurDAO.save(utilisateur);
 	}
 
 }
---
+++ ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/dal/UtilisateurDAOSql.java
@@ -0,0 +1,16 @@
+package fr.eni.groupe1.encheres.dal;
+
+import org.springframework.stereotype.Repository;
+
+import fr.eni.groupe1.encheres.bo.Utilisateur;
+
+@Repository
+public class UtilisateurDAOSql implements UtilisateurDAO {
+
+	@Override
+	public void save(Utilisateur utilisateur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
