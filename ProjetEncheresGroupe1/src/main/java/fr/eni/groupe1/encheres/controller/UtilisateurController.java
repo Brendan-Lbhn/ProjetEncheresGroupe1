a708be5eb488928818349869ac2f59e707b89f97
tgarciapardo2023 <tgarciapardo2023@12SE22-CKLRZS2.ad.campus-eni.fr>
2023-06-28 10:03:35 +0200

    Utilisateur s'écrit bien dans la bd

--- ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/controller/UtilisateurController.java
+++ ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/controller/UtilisateurController.java
@@ -1,5 +1,6 @@
 package fr.eni.groupe1.encheres.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +13,7 @@ import fr.eni.groupe1.encheres.bo.Utilisateur;
 public class UtilisateurController {
 	private UtilisateurService utilisateurService;
 	
+	@Autowired
 	public UtilisateurController(UtilisateurService utilisateurService) {
 		this.utilisateurService=utilisateurService;
 	}
