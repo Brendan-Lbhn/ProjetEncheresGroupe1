fd634c6f1be2dc94964e6ed1fbfea7f6ddd40b4e
jmiossec2023 <jmiossec2023@12SE22-CFLRZS2.ad.campus-eni.fr>
2023-06-29 15:04:15 +0200

    affichage profil : OK

--- ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/controller/UtilisateurController.java
+++ ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/controller/UtilisateurController.java
@@ -1,6 +1,5 @@
 package fr.eni.groupe1.encheres.controller;
 
-
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import fr.eni.groupe1.encheres.bll.UtilisateurService;
 import fr.eni.groupe1.encheres.bo.Utilisateur;
@@ -20,34 +15,36 @@ import fr.eni.groupe1.encheres.bo.Utilisateur;
 @Controller
 public class UtilisateurController {
 	private UtilisateurService utilisateurService;
-	
+
 	@Autowired
 	public UtilisateurController(UtilisateurService utilisateurService) {
-		this.utilisateurService=utilisateurService;
+		this.utilisateurService = utilisateurService;
 	}
-	/////////////////////////////////        CREATION D UN PROFIL     ////////////////////////////////////////////
-	
-	@GetMapping({"/CreationProfil"})
-	public String vueCreationProfil( Utilisateur utilisateur) {
+	///////////////////////////////// CREATION D UN PROFIL
+	///////////////////////////////// ////////////////////////////////////////////
+
+	@GetMapping({ "/CreationProfil" })
+	public String vueCreationProfil(Utilisateur utilisateur) {
 		return "CreationProfil";
 	}
-	@PostMapping({"/CreationProfil/Suite"})
-	public String ajouterUtilisateur(@ModelAttribute("utilisateur")Utilisateur utilisateur) {
+
+	@PostMapping({ "/CreationProfil/Suite" })
+	public String ajouterUtilisateur(@ModelAttribute("utilisateur") Utilisateur utilisateur) {
 		utilisateurService.enregistrer(utilisateur);
 		return "redirect:/";
-		
+
 	}
-	/////////////////////////////////        CREATION D UN ARTICLE     ////////////////////////////////////////////
-	@GetMapping({"/ProfilUtilisateur"})
-	@ResponseBody
-	public String afficherProfil (Principal principal) {
+
+	///////////////////////////////// CREATION D UN ARTICLE
+	///////////////////////////////// ////////////////////////////////////////////
+	@GetMapping({ "/ProfilUtilisateur" })
+	public String afficherProfil(Principal principal, Model model) {
 		System.out.println("je passe par le get profil");
-	 principal.getName();
-	 
+		principal.getName();
+
 		System.out.println(principal.getName());
-		
-		Utilisateur utilisateurProfil = utilisateurService.findByPseudo(principal.getName());
-			
+		model.addAttribute("Utilisateur",utilisateurService.findByPseudo(principal.getName()));
+
 		return "ProfilUtilisateur";
 	}
 }
