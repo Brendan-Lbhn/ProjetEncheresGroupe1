3b8c3992391d09528465a19f7b303c4466f53d31
Garcia-Pardo Thomas <137761880+Barsth@users.noreply.github.com>
2023-06-29 09:52:29 +0200

    Security Commit

--- ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/bll/UtilisateurServiceImpl.java
+++ ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/bll/UtilisateurServiceImpl.java
@@ -1,15 +1,8 @@
 package fr.eni.groupe1.encheres.bll;
 
-<<<<<<< HEAD
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.*;
-import org.springframework.stereotype.*;
-=======
 import java.util.List;
-
 import org.springframework.stereotype.Service;
->>>>>>> branch 'main' of https://github.com/Brendan-Lbhn/ProjetEncheresGroupe1.git
-
 import fr.eni.groupe1.encheres.bo.Utilisateur;
 import fr.eni.groupe1.encheres.dal.UtilisateurDAO;
 
@@ -18,7 +11,6 @@ public class UtilisateurServiceImpl implements UtilisateurService {
     private UtilisateurDAO utilisateurDAO;
     private PasswordEncoder passwordEncoder;
 
-<<<<<<< HEAD
     public UtilisateurServiceImpl(UtilisateurDAO utilisateurDAO, PasswordEncoder passwordEncoder) {
         this.utilisateurDAO = utilisateurDAO;
         this.passwordEncoder = passwordEncoder;
@@ -29,11 +21,6 @@ public class UtilisateurServiceImpl implements UtilisateurService {
         utilisateur.setMotDePasse(passwordEncoder.encode(utilisateur.getMotDePasse()));
         utilisateurDAO.save(utilisateur);
     }
-=======
-	@Override
-	public void enregistrer(Utilisateur utilisateur) {
-		utilisateurDAO.save(utilisateur);
-	}
 
 	@Override
 	public List<Utilisateur> getUtilisateur() {
@@ -47,5 +34,4 @@ public class UtilisateurServiceImpl implements UtilisateurService {
 		return utilisateurDAO.findById(id);
 	}
 
->>>>>>> branch 'main' of https://github.com/Brendan-Lbhn/ProjetEncheresGroupe1.git
 }
