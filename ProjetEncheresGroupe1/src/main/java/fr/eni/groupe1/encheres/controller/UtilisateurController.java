a9942d94531046dd32ab0f762b279b47a7f3cd9e
Garcia-Pardo Thomas <137761880+Barsth@users.noreply.github.com>
2023-06-29 15:09:02 +0200

    Modification UtilisateurController pour la validation

--- ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/controller/UtilisateurController.java
+++ ProjetEncheresGroupe1/src/main/java/fr/eni/groupe1/encheres/controller/UtilisateurController.java
@@ -5,12 +5,14 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import fr.eni.groupe1.encheres.bll.UtilisateurService;
 import fr.eni.groupe1.encheres.bo.Utilisateur;
+import jakarta.validation.Valid;
 
 @Controller
 public class UtilisateurController {
@@ -28,11 +30,15 @@ public class UtilisateurController {
 		return "CreationProfil";
 	}
 
-	@PostMapping({ "/CreationProfil/Suite" })
-	public String ajouterUtilisateur(@ModelAttribute("utilisateur") Utilisateur utilisateur) {
-		utilisateurService.enregistrer(utilisateur);
-		return "redirect:/";
-
+	@PostMapping({"/CreationProfil/Suite"})
+	public String ajouterUtilisateur(@Valid @ModelAttribute("utilisateur") Utilisateur utilisateur, BindingResult bindingResult) {
+	    if (bindingResult.hasErrors()) {
+	        // Gérer les erreurs de validation ici
+	        return "CreationProfil";
+	    }
+	    
+	    utilisateurService.enregistrer(utilisateur);
+	    return "redirect:/";
 	}
 
 	///////////////////////////////// CREATION D UN ARTICLE
