983ad459511714f6dd6f14dcd480df3e3ceaaafb
blebihan2023 <blebihan2023@12SE22-3TJRZS2.ad.campus-eni.fr>
2023-07-06 09:38:45 +0200

    désactivation des filtres

--- ProjetEncheresGroupe1/src/main/resources/static/js/script.js
+++ ProjetEncheresGroupe1/src/main/resources/static/js/script.js
@@ -1,27 +1,37 @@
-window.onload = init()
+window.onload = init;
 
-function init(){
-    document.getElementById('achats').addEventListener('change',desactiverVentes)
-    document.getElementById('ventes').addEventListener('change',desactiverAchats)
-	console.log("coucou")
-	}
+function init() {
+    var achatsRadio = document.getElementById('achats');
+    var ventesRadio = document.getElementById('ventes');
 
-function desactiverVentes(){
-	document.getElementById('encheres-ouvertes').setAttribute('disabled', true)
-	document.getElementById('encheres-en-cours').setAttribute('disabled', true)
-	document.getElementById('encheres-remportees').setAttribute('disabled', true)
-	document.getElementById('ventes-en-cours').setAttribute('disabled', false)
-	document.getElementById('ventes-non-debutees').setAttribute('disabled', false)
-	document.getElementById('ventes-terminees').setAttribute('disabled', false)
-	console.log("coucou")
+    achatsRadio.addEventListener('change', handleRadioButtonChange);
+    ventesRadio.addEventListener('change', handleRadioButtonChange);
+    console.log("coucou");
 }
 
-function desactiverAchats(){
-	document.getElementById('encheres-ouvertes').setAttribute('disabled', false)
-	document.getElementById('encheres-en-cours').setAttribute('disabled', false)
-	document.getElementById('encheres-remportees').setAttribute('disabled', false)
-	document.getElementById('ventes-en-cours').setAttribute('disabled', true)
-	document.getElementById('ventes-non-debutees').setAttribute('disabled', true)
-	document.getElementById('ventes-terminees').setAttribute('disabled', true)
-	console.log("coucou")
+function handleRadioButtonChange(event) {
+    var achatsRadio = document.getElementById('achats');
+    var ventesRadio = document.getElementById('ventes');
+    var encheresOuvertes = document.getElementById('encheres-ouvertes');
+    var encheresEnCours = document.getElementById('encheres-en-cours');
+    var encheresRemportees = document.getElementById('encheres-remportees');
+    var ventesEnCours = document.getElementById('ventes-en-cours');
+    var ventesNonDebutees = document.getElementById('ventes-non-debutees');
+    var ventesTerminees = document.getElementById('ventes-terminees');
+
+    if (achatsRadio.checked) {
+        encheresOuvertes.disabled = false;
+        encheresEnCours.disabled = false;
+        encheresRemportees.disabled = false;
+        ventesEnCours.disabled = true;
+        ventesNonDebutees.disabled = true;
+        ventesTerminees.disabled = true;
+    } else if (ventesRadio.checked) {
+        encheresOuvertes.disabled = true;
+        encheresEnCours.disabled = true;
+        encheresRemportees.disabled = true;
+        ventesEnCours.disabled = false;
+        ventesNonDebutees.disabled = false;
+        ventesTerminees.disabled = false;
+    }
 }
