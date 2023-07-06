51bfb8abd0d9d85c5ed803579cbe83f951bd1df5
blebihan2023 <blebihan2023@12SE22-3TJRZS2.ad.campus-eni.fr>
2023-07-06 11:53:06 +0200

    modifs page d'accueil

--- ProjetEncheresGroupe1/src/main/resources/static/js/script.js
+++ ProjetEncheresGroupe1/src/main/resources/static/js/script.js
@@ -3,7 +3,7 @@ window.onload = init;
 function init() {
     var achatsRadio = document.getElementById('achats');
     var ventesRadio = document.getElementById('ventes');
-
+    handleRadioButtonChange();
     achatsRadio.addEventListener('change', handleRadioButtonChange);
     ventesRadio.addEventListener('change', handleRadioButtonChange);
     console.log("coucou");
@@ -18,6 +18,12 @@ function handleRadioButtonChange(event) {
     var ventesEnCours = document.getElementById('ventes-en-cours');
     var ventesNonDebutees = document.getElementById('ventes-non-debutees');
     var ventesTerminees = document.getElementById('ventes-terminees');
+    var txtEncheresOuvertes = document.getElementById('encheres-ouvertes-txt');
+    var txtEncheresEnCours = document.getElementById('encheres-en-cours-txt');
+    var txtEncheresRemportees = document.getElementById('encheres-remportees-txt');
+    var txtVentesEnCours = document.getElementById('ventes-en-cours-txt');
+    var txtVentesNonDebutees = document.getElementById('ventes-non-debutees-txt');
+    var txtVentesTerminees = document.getElementById('ventes-terminees-txt');
 
     if (achatsRadio.checked) {
         encheresOuvertes.disabled = false;
@@ -26,6 +32,13 @@ function handleRadioButtonChange(event) {
         ventesEnCours.disabled = true;
         ventesNonDebutees.disabled = true;
         ventesTerminees.disabled = true;
+        txtVentesEnCours.setAttribute('class','option-grisee')
+        txtVentesNonDebutees.setAttribute('class','option-grisee')
+        txtVentesTerminees .setAttribute('class','option-grisee')
+        txtEncheresOuvertes.setAttribute('class','option-valide') 
+        txtEncheresEnCours.setAttribute('class','option-valide')
+        txtEncheresRemportees.setAttribute('class','option-valide')
+        
     } else if (ventesRadio.checked) {
         encheresOuvertes.disabled = true;
         encheresEnCours.disabled = true;
@@ -33,5 +46,12 @@ function handleRadioButtonChange(event) {
         ventesEnCours.disabled = false;
         ventesNonDebutees.disabled = false;
         ventesTerminees.disabled = false;
+        txtEncheresOuvertes.setAttribute('class','option-grisee') 
+        txtEncheresEnCours.setAttribute('class','option-grisee')
+        txtEncheresRemportees.setAttribute('class','option-grisee')
+       	txtVentesEnCours.setAttribute('class','option-valide')
+        txtVentesNonDebutees.setAttribute('class','option-valide')
+        txtVentesTerminees .setAttribute('class','option-valide')
+        
     }
 }
